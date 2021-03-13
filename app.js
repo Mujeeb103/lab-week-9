267fe8e3fe529317c95de1209342a8c769d5a0c9
Mujeeb Choudhry <mujeeb9019@gmail.com>
2021-03-12 21:08:32 -0800

    week 9 lab

---
+++ app.js
@@ -0,0 +1,31 @@
+let x = 5
+x
+5
+
+let mystring = '10ABC'
+mystring
+"10ABC"
+
+const number = 25
+number
+25
+
+const mujeebisnice = false
+mujeebisnice
+false
+
+if (x === number) {
+    result = 'they match'
+  } else {
+    result = 'no luck'
+  }
+  
+  "no luck"
+
+  for (letter of mystring)
+  console.log(letter)
+  1 
+  0 
+  A 
+  B 
+  C
